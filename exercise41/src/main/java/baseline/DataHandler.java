11766e0bd4ccbc336733b01d5712ebf85ee2f303
Jyke321 <jyke321@gmail.com>
2021-10-07 23:44:39 -0400

    ex41 pseudo

---
+++ exercise41/src/main/java/baseline/DataHandler.java
@@ -0,0 +1,21 @@
+package baseline;
+
+import java.awt.*;
+
+public class DataHandler {
+    //I haven't done any reading yet so...
+    List data = new List();
+    public void getDataFromFile() {
+        //get a line from file and store it in data as a string
+        // (loop until end of file, as long as it is similar to C)
+    }
+    public void displayNumberOfNames() {
+        //print the number of elements in the list data
+    }
+    public void displayData() {
+        //loops through list and displays the data
+    }
+    public void sortDataAlphabetically() {
+        //sort the data so that it is in numerical order
+    }
+}
---
+++ exercise41/src/main/java/baseline/Solution41.java
@@ -0,0 +1,19 @@
+package baseline;
+
+/*
+ *  UCF COP3330 Fall 2021 Assignment 4 Solutions
+ *  Copyright 2021 Jacob Cordonero
+ */
+
+public class Solution41 {
+    public static void main(String[] args) {
+        //get the data from the file
+
+        //sort the data from the file by last name alphabetically
+
+        //print the total number of names
+
+        //print the data from the file
+
+    }
+}
