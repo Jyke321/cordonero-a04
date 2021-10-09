312f25f0b4142e0de5a642b8789ca4a92928b070
Jyke321 <jyke321@gmail.com>
2021-10-08 20:23:43 -0400

    ex43 pseudo

---
+++ exercise43/src/main/java/baseline/DataHandler.java
@@ -0,0 +1,29 @@
+package baseline;
+
+import java.util.Scanner;
+
+public class DataHandler {
+    //user input variables
+    private String siteName;
+    private String author;
+    private Boolean js;  //act as flag for y/n
+    private Boolean css; //act as flag for y/n
+
+    private static final Scanner in = new Scanner(System.in);
+
+    public void getUserInput() {
+        //prompt user for input and set the appropriate variables
+
+    }
+
+    public void generateUserFiles() {
+        //uses the data to make the website directory
+
+        //uses the data to make the index.html
+
+        //make the js file is asked for
+
+        //make the css file if asked for
+
+    }
+}
---
+++ exercise43/src/main/java/baseline/Solution43.java
@@ -0,0 +1,15 @@
+package baseline;
+
+/*
+ *  UCF COP3330 Fall 2021 Assignment 4 Solutions
+ *  Copyright 2021 Jacob Cordonero
+ */
+
+public class Solution43 {
+    public static void main(String[] args) {
+        //get user input
+
+        //create requested files following user specifications
+
+    }
+}
