bcf47e67359363002118c2a9c1ecf6bc3728a581
Jyke321 <jyke321@gmail.com>
2021-10-16 18:57:48 -0400

    ex44 pseudo

---
+++ exercise44/src/main/java/baseline/DataHandler.java
@@ -0,0 +1,27 @@
+package baseline;
+
+import java.util.HashMap;
+import java.util.List;
+
+public class DataHandler {
+    private List<HashMap<String,String>> data = new List<HashMap<>>();
+
+    public void getDataFromJSON() {
+        //parseDataFromJSON and store in data
+
+    }
+    private boolean doesProductExist() {
+        //determines whether string exists in data
+        return false;
+    }
+    private void displayProduct() {
+        //displays the product information
+
+    }
+    public void findProduct() {
+        //prompts the user for product name
+
+        //if product exists in data, display product
+
+    }
+}
---
+++ exercise44/src/main/java/baseline/Solution44.java
@@ -0,0 +1,17 @@
+package baseline;
+
+/*
+ *  UCF COP3330 Fall 2021 Assignment 4 Solutions
+ *  Copyright 2021 Jacob Cordonero
+ */
+
+public class Solution44 {
+    //I have no clue how json works and I feel as though researching that defeats the point of pseduocode
+    //more realistically I probably just have no clue what the point of pseudo code is so oh well.
+    public static void main(String[] args) {
+        //call an object to store the json data
+
+        //either use a loop to call a function or just a call a self containted object method
+
+    }
+}
