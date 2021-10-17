55852aa540c87a3584061d4c5ee41afa3ed44108
Jyke321 <jyke321@gmail.com>
2021-10-16 21:07:39 -0400

    ex44 done

--- exercise44/src/main/java/baseline/DataHandler.java
+++ exercise44/src/main/java/baseline/DataHandler.java
@@ -4,9 +4,13 @@ import com.google.gson.Gson;
 import com.google.gson.stream.JsonReader;
 
 import java.io.*;
+import java.text.DecimalFormat;
+import java.util.Scanner;
 
 public class DataHandler {
+    private static final Scanner input = new Scanner(System.in);
     private Data data;
+    private int productIndex;
 
     public void getDataFromJSON() {
         //parseDataFromJSON and store in data
@@ -21,18 +25,39 @@ public class DataHandler {
     public String getNameFromSecondProduct(){
         return  data.products[1].name;
     }
-    private boolean doesProductExist() {
+    private boolean doesProductExist(String searchKey) {
         //determines whether string exists in data
+        for (int i=0; i<data.products.length; i++) {
+            if(data.products[i].name.compareTo(searchKey)==0) {
+                productIndex = i;
+                return true;
+            }
+        }
         return false;
     }
     private void displayProduct() {
+        //format the price as money
+        DecimalFormat df = new DecimalFormat();
+        df.setMinimumFractionDigits(2);
+        df.setMaximumFractionDigits(2);
         //displays the product information
-
+        System.out.println("Name: " + data.products[productIndex].name);
+        System.out.println("Price: " + df.format(data.products[productIndex].price));
+        System.out.println("Quantity: " + data.products[productIndex].quantity);
     }
     public void findProduct() {
         //prompts the user for product name
-
-        //if product exists in data, display product
-
+        boolean foundProduct = false;
+        while (!foundProduct) {
+            System.out.print("What is the product name? ");
+            String searchKey = input.nextLine();
+            //if product exists in data, display product
+            if (doesProductExist(searchKey)) {
+                foundProduct = true;
+                displayProduct();
+            } else {
+                System.out.println("Sorry, that product was not found in our inventory.");
+            }
+        }
     }
 }
--- exercise44/src/test/java/baseline/DataHandlerTest.java
+++ exercise44/src/test/java/baseline/DataHandlerTest.java
@@ -5,12 +5,11 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class DataHandlerTest {
+    static DataHandler data = new DataHandler();
     @Test
     void testGetNameFromSecondProduct() {
-        DataHandler data = new DataHandler();
         data.getDataFromJSON();
         String actual = "Thing";
         assertEquals(actual,data.getNameFromSecondProduct());
     }
-
 }
